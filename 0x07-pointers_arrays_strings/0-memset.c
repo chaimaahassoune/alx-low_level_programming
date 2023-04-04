8f051c93b627a39c8b78a5b6ec510f56a03fb8ab
chaimaahassoune <85140826+chaimaahassoune@users.noreply.github.com>
2023-04-04 01:50:10 +0000

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 /**
  * Sets the first n bytes of the memory area pointed to by s to the constant byte b.
  *
- * @param s A pointer to the memory area to fill.
- * @param b The value to set each byte to.
- * @param n The number of bytes to fill.
+ * @s: A pointer to the memory area to fill
+ * @b: The value to set each byte to.
+ * @n: The number of bytes to fill.
  *
- * @return A pointer to the memory area s.
+ * Return: A pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
