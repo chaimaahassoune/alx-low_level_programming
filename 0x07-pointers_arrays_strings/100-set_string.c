20893fa8c65f25cca0d828df47896ff459206315
chaimaahassoune <chaimaa.22hassoune@gmail.com>
2023-04-03 19:54:55 -0700

    test

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+/**
+ * set_string - sets the value of a pointer to a char
+ *
+ * @s: double pointer to the string to be modified
+ * @to: pointer to the new string
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+    *s = to;
+}
