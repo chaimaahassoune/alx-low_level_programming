fb2245c21215d1e9ffbab06473ab9ffb6c32061e
chaimaahassoune <85140826+chaimaahassoune@users.noreply.github.com>
2023-04-04 02:25:22 +0000

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  *_strchr - Locates the first occurrence of the character c in the string s
  *
@@ -7,15 +9,22 @@
  *
  * Return A pointer to the first occurrence of the character c in the string s
  */
-char* _strchr(char* s, char c) {
-    while (*s != '\0') {
-        if (*s == c) {
+char* _strchr(char* s, char c)
+{ 
+    
+    while (*s != '\0') 
+    {
+        if (*s == c)
+        {
             return s;
         }
         s++;
     }
-    if (c == '\0') {
+    
+    if (c == '\0') 
+    {
         return s;
     }
+    
     return NULL;
 }
