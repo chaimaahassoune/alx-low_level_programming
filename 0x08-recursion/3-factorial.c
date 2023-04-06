44452151832b0d4944f4fa3c1e0bfbd8dbb2c29a
chaimaahassoune <chaimaa.22hassoune@gmail.com>
2023-04-05 18:52:51 -0700

    recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,12 +6,12 @@
  *
  * Return: the result
  */
-int factorial(int n)
+int factorial (int n)
 {
 	if (n < 0)
-		return(-1);
+		return (-1);
 	else if (n == 0)
-		return(1);
+		return (1);
 	else
-		return(n * factorial(n - 1));
+		return (n * factorial(n - 1));
 }
