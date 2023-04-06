dcf7f451c39d06f362b16d6d4d712971799c0597
chaimaahassoune <85140826+chaimaahassoune@users.noreply.github.com>
2023-04-06 01:08:56 +0000

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,8 +4,7 @@
  * _puts_recursion - prints a string,followed by a new line
  * @s: pointer to a string
  *
- * Return: It return void 
- *
+ * Return: It return void
  */
 void _puts_recursion(char *s)
 {
@@ -13,11 +12,10 @@ void _puts_recursion(char *s)
 	{
 		_putchar('\n');
 		return;
-	} 
+	}
 
 
 	if (*s != '\0')
 		_putchar(*s);
-	
 	_puts_recursion(s + 1);
 }
