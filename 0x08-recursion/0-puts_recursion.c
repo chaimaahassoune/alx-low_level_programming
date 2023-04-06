b7f64ce9b085deaadebf7f9c3b32f718ad32f664
chaimaahassoune <85140826+chaimaahassoune@users.noreply.github.com>
2023-04-06 00:40:37 +0000

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -14,8 +14,9 @@ void _puts_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
-
-	_putchar(*s);
+        
+	if (*s != '\0')
+	       _putchar(*s);
 	
-	_puts_recursion(s+1);
+	_puts_recursion(s + 1);
 }
