068930fbc28230f001de3132fd49eb70546eb87a
chaimaahassoune <chaimaa.22hassoune@gmail.com>
2023-04-09 21:56:29 -0700

    argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ * @argc: the number of command-line arguments passed
+ * @argv: an ary of strings containing the command-line arguments
+ *
+ * return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
