fd8ff5a6690a1fb5591830affbb8efec4077a490
chaimaahassoune <chaimaa.22hassoune@gmail.com>
2023-04-13 19:06:03 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - allocate memory for an array
+ * @nmemb: Number of elements
+ * @size: the size
+ *
+ * Return: pointer or null on failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *tab;
+	unsigned int i,k;
+
+	k = nmemb * size;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	tab = malloc(k);
+
+	if (tab == NULL)
+		return (NULL);
+
+	for (i = 0; i < k; i++)
+		tab[i] = 0;
+
+	return ((void *)tab);
+}
