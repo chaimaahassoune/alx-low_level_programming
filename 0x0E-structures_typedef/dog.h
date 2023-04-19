7e56726a32bac3c3d66c3a4d94928ceabbf2fa1f
chaimaahassoune <85140826+chaimaahassoune@users.noreply.github.com>
2023-04-19 01:52:04 +0000

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,10 +15,12 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} my_dog;
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-my_dog *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 
 #endif /* DOG_H_FILE */
