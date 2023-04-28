2844621f9e8f1689e168aefc245fc8a068bf60fa
chaimaahassoune <chaimaa.22hassoune@gmail.com>
2023-04-27 19:26:51 -0700

    test

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,9 @@
-section .data
-    hello db 'Hello, Holberton', 0x0A, 0x00
-
-section .text
-    global main
-    extern printf
-
+          global    main
+          extern    printf
 main:
-    push rbp
-    mov rbp, rsp
-
-    lea rdi, [hello]
-    mov rsi, 0
-    call printf
-
-    mov rsp, rbp
-    pop rbp
-    ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
