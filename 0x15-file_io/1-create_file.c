599f7477618b58358b079512f49565bb00365ea0
chaimaahassoune <chaimaa.22hassoune@gmail.com>
2023-05-08 17:08:47 -0700

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		while (text_content[length] != '\0')
-			lengh++;
+			length++;
 	}
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
